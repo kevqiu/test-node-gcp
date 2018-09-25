24dacab191467ce60455a03ded954cfa49a6aa76
Kevin Qiu <kevinqiu96@gmail.com>
2018-09-25 16:52:32 -0700

    adding 2

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const PORT = 8080;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/api/test', (req, res) => res.json({ test: 'hello' }));
+app.get('/api/test/2', (req, res) => res.json({ test: 'hello 2' }));
 
 app.listen(PORT, console.log(`App listening on port ${PORT}`));
